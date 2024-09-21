eec5a3a3b1ea98caaf34984b8bd25eb987cb0e50
RusTantas <165479418+RusTantas@users.noreply.github.com>
2024-09-21 13:38:34 +0300

    Add files via upload

--- modul_14/BOT_Continue/crud_functions.py
+++ modul_14/BOT_Continue/crud_functions.py
@@ -5,6 +5,8 @@ from urllib.parse import uses_query
 
 connection = sqlite3.connect(database="database_r.db")
 cursor = connection.cursor()
+connection_2 = sqlite3.connect(database="database_users.db")
+cursor_2 = connection_2.cursor()
 
 def initiate_db():
     cursor.execute('''
@@ -15,9 +17,26 @@ def initiate_db():
     price INT
     );
     ''')
+    cursor_2.execute( '''
+    CREATE TABLE IF NOT EXISTS Users (
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL
+    );    '''
+    )
+    connection.commit()
+    connection.close()
+    connection_2.commit()
+    connection_2.close()
+
+
 
 
 def get_all_products():
+    connection = sqlite3.connect(database="database_r.db")
+    cursor = connection.cursor()
     cursor.execute("SELECT * FROM Products ")
     name = cursor.fetchall()
 
@@ -34,6 +53,19 @@ def make_maks(title, description, price):
     connection.commit()
     connection.close()
 
+def add_user(username, email, age, balans):
+    connection_2 = sqlite3.connect(database="database_users.db")
+    cursor_2 = connection_2.cursor()
+    check_user = cursor_2.execute("SELECT * FROM Users WHERE username=?", (username,))
+    if check_user.fetchone() is None:
+        cursor_2.execute(
+                         "INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)", (username,email,age, balans)
+        )
+        a =True
+    else: a =False
+    connection_2.commit()
+    connection_2.close()
+    return a
 
 
 
