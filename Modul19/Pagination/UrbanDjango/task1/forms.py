31adf3ce7a6e70d3ab5adbeab9d612e7b7f53d62
RusTantas <165479418+RusTantas@users.noreply.github.com>
2024-11-09 18:01:36 +0300

    Delete Modul19/Pagination/UrbanDjango directory
    123

--- Modul19/Pagination/UrbanDjango/task1/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class ContactForm(forms.Form):
-    username = forms.CharField(max_length=30, label='Ваше логин')
-    password = forms.CharField(max_length=8, label='Ваше пароль')
-    password_2 = forms.CharField(max_length=8, label='Введите пароль повторно:')
-    age = forms.IntegerField(label='Введите ваш возраст')
