f570bd3347abe26216aa9f0b055b7f126a5bb0e4
RusTantas <165479418+RusTantas@users.noreply.github.com>
2024-11-15 18:34:11 +0300

    Create module_9_1.py

---
+++ Dolgi/module_9_1.py
@@ -0,0 +1,29 @@
+def apply_all_func(int_list, *functions):
+    results = {}
+    
+    for func in functions:
+        # Получаем название функции
+        func_name = func.__name__
+        # Применяем функцию к списку и сохраняем результат в словаре
+        results[func_name] = func(int_list)
+    
+    return results
+
+
+def sum_numbers(numbers):
+    return sum(numbers)
+
+def max_number(numbers):
+    return max(numbers)
+
+def min_number(numbers):
+    return min(numbers)
+
+def average(numbers):
+    return sum(numbers) / len(numbers) if numbers else 0
+
+
+int_list = [1, 2, 3, 4, 5]
+results = apply_all_func(int_list, sum_numbers, max_number, min_number, average)
+
+print(results)
